1f0c7c3eba8e3e553a9a53c2cdf724fc763cddf6
christophbunte <christophbunte@gmail.com>
2018-11-04 19:16:01 +0100

    more work

--- rplugin/python3/scala-vim.py
+++ rplugin/python3/scala-vim.py
@@ -30,7 +30,7 @@ class ScalaVim(object):
                 self.nvim.err_write('scala-vim: failed to connect'
                                     'to completion engine...' + '\n')
 
-    @neovim.command('ScalaType', sync=True)
+    @neovim.command('ScalaType')
     def get_type(self):
         window = self.nvim.current.window
         cursor = window.cursor
@@ -39,9 +39,19 @@ class ScalaVim(object):
         tpe = self.engine.askTypeAt('current_buffer', '\n'.join(buf), offset)
         self.nvim.out_write(tpe + '\n')
 
+    @neovim.command('ScalaErrors')
+    def get_errors(self):
+        errors = self.engine.getErrors()
+        self.nvim.err_write(errors + '\n')
+
     @neovim.autocmd('BufEnter', pattern='*.scala')
-    def initialize(self):
+    def on_buf_enter(self):
         self._initialize()
+
+    @neovim.autocmd('BufWritePost', pattern='*.scala')
+    def after_buffer_write(self):
+        buf = self.nvim.current.buffer
+        self.engine.reloadFile('current_buffer', '\n'.join(buf))
 
     @neovim.command('TestCommand', nargs='*', range='')
     def testcommand(self, args, range):
